d1e7fe77ca6939184316b83b212233fa39a4ebc6
nj314 <betweenthieves@gmail.com>
2020-09-12 16:31:55 -0500

    Adds jest with a basic gatsby test example

---
+++ src/components/header.test.js
@@ -0,0 +1,14 @@
+// Example test
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Header from "./header"
+
+describe("Header", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Header siteTitle="Default Starter" />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
