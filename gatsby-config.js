0c91292bc685ccfc4d3beb90284e24838fff8913
nj314 <betweenthieves@gmail.com>
2020-09-12 15:33:17 -0500

    Adjusts build config to prefix gatsby paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  pathPrefix: "/echoes-industry-planner",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
