c2a966de7c70d1f97306ed84963343267c17933a
nj314 <betweenthieves@gmail.com>
2020-09-14 08:23:10 -0500

    Basic project config updates

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # echoes-industry-planner
-A planning tool for industrialists in Eve Echoes.
+
+A planning tool for industrialists in Eve Echoes. Produces estimates about profits/losses based on the player's current inventory, reprocessing skills, manufacturing skills, and market data.
+
+## Powered by the community
+
+This tool is intended to be open to PRs from the community for blueprint corrections, additions, and any other enhancements as the game receives updates over time. The `game-model` module contains all BP and relevant skill definitions, and should make it easy to submit PRs in response to game patches.
+
+## Deployment
+
+Branch `master` is continuously-deployed to Github Pages at https://nj314.github.io/echoes-industry-planner/
+
+## Market data
+
+Unfortunately, market data input is all manual as CCP hasn't yet made a public API for Echoes market data. When this changes, open an issue!
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Echoes Industry Planner`,
+    description: `A planning tool for industrialists in Eve Echoes.`,
+    author: `@nj314`,
   },
   pathPrefix: "/echoes-industry-planner",
   plugins: [
@@ -12,6 +12,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `game-model`,
+        path: `${__dirname}/game-model`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -32,4 +39,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
